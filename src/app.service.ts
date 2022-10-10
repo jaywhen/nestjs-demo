8ce77bc3be0a98d8c047d3e050d375893c760306
jaywhen <jaywhenx@gmail.com>
2022-10-11 00:12:59 +0800

    using chalk in <ts4.6 && import

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import * as chalk from 'chalk';
 
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log(chalk.blue.bgGray('this is red string'))
     return 'Hello World!';
   }
 }
